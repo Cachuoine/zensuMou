8fc1c83e603083d54d1fe52bc8547f82d08cc3a3
Fish <daohuylam2205@gmail.com>
2026-07-28 00:32:58 +0700

    Update Config.lua

---
+++ FishHub/FishHubCollect/Games/BloxFruit/Config.lua
@@ -0,0 +1,9 @@
+-- Games/BloxFruit/Config.lua
+local BloxFruitConfig = {
+    GameName = "Blox Fruit",
+    SupportedPlaceIds = {2753915549, 4442272183, 7449423635}, -- Các Place ID của Sea 1, 2, 3
+    DefaultFarmSpeed = 16,
+    AutoLoadFeatures = true
+}
+
+return BloxFruitConfig
