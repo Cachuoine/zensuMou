8103b352af15383c2e20c1379a8e311d2c75ebeb
Fish <daohuylam2205@gmail.com>
2026-07-28 19:58:17 +0700

    Delete FishHub/FishHubCollect directory

--- FishHub/FishHubCollect/Games/BloxFruit/Config.lua
+++
@@ -1,9 +0,0 @@
--- Games/BloxFruit/Config.lua
-local BloxFruitConfig = {
-    GameName = "Blox Fruit",
-    SupportedPlaceIds = {2753915549, 4442272183, 7449423635}, -- Các Place ID của Sea 1, 2, 3
-    DefaultFarmSpeed = 16,
-    AutoLoadFeatures = true
-}
-
-return BloxFruitConfig
--- FishHub/FishHubCollect/Games/Default.lua
+++
@@ -1,8 +0,0 @@
--- Games/Default.lua
-local DefaultGame = {}
-
-function DefaultGame.Init()
-    print("[FishHub] Game này chưa được hỗ trợ riêng. Chạy chế độ mặc định.")
-end
-
-return DefaultGame
