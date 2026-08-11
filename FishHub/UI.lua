b462cc98fa4f692d032cb3b75cfed6b9de9a2a0f
Fish <daohuylam2205@gmail.com>
2026-08-11 16:43:19 +0700

    Update UI.lua

--- FishHub/UI.lua
+++ FishHub/UI.lua
@@ -2431,8 +2431,8 @@ contentContainer.BackgroundTransparency = 1
 local tabs = {}
 
 
-local HOME_URL = "https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/Home.lua"
-local FUNCTION_URL = "https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/Function.lua"
+local HOME_URL = "https://raw.githubusercontent.com/Cachuoine/zensuMou/refs/heads/main/FishHub/Home.lua"
+local FUNCTION_URL = "hhttps://raw.githubusercontent.com/Cachuoine/zensuMou/refs/heads/main/FishHub/Function.lua"
 
 local RemoteCache = {}
 local RemoteLoading = {}
