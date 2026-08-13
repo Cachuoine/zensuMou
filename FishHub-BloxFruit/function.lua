bf0a0dc35e46cd1b6d46bcadb3f98850bd8d9c57
Fish <daohuylam2205@gmail.com>
2026-08-13 07:02:20 +0700

    Update function.lua

---
+++ FishHub-BloxFruit/function.lua
@@ -0,0 +1,5 @@
+return function(ctx)
+    -- Function is intentionally kept as an independent module.
+    -- Add/replace function cards here without touching FishHub_Main.lua.
+    ctx.TabFrame.CanvasSize = UDim2.new(0, 0, 0, 0)
+end
