1e9f2e65667df2d30c8a7efe899eee1c87e00181
Fish <daohuylam2205@gmail.com>
2026-08-31 08:31:11 +0700

    Update function.lua

--- FishHub-BloxFruit/function.lua
+++ FishHub-BloxFruit/function.lua
@@ -180,7 +180,7 @@ loadFunction = function()
         {"SETTING FARM", "settingfarm", "Farming preferences."},
         {"FARM", "farm", "Farming functions and controls."},
         {"STACK FARM", "stackfarm", "Stack farm and quest utilities."},
-        {"ISLAND", "island", "Island travel and navigation."},
+        {"TELEPORT", "teleport", "Island travel and navigation."},
         {"PLAYER", "player", "Player utilities and helpers."},
         {"FRUIT", "fruit", "Fruit utilities and helpers."},
         {"SETTING", "setting", "FishHub settings and controls."},
